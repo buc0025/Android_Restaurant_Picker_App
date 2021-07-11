794cfee4420cc0ebc1e523f0eca6089669aa14b8
buc0025 <stanley.yang001@gmail.com>
2021-07-11 15:29:33 -0400

    Registration screen is now connected to Firebase Authentication

--- app/src/main/java/com/example/restaurantpicker/RegistrationActivity.java
+++ app/src/main/java/com/example/restaurantpicker/RegistrationActivity.java
@@ -1,12 +1,20 @@
 package com.example.restaurantpicker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
@@ -27,5 +35,50 @@ public class RegistrationActivity extends AppCompatActivity {
         registerBtn = findViewById(R.id.registerBtn);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        // Check if user is already logged in. Send them to main activity if they are logged in.
+        if (firebaseAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edtEmail.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+
+                // email and password cannot be empty in order to register
+                if (TextUtils.isEmpty(email)) {
+                    edtEmail.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    edtPassword.setError("Password is required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    edtPassword.setError("Password must be at least 6 characters.");
+                    return;
+                }
+
+                // Create user in Firebase
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "User created", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
