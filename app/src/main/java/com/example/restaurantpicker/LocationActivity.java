db7f2d6fee21d30ab77672c18cb3aa0ace244d1e
buc0025 <stanley.yang001@gmail.com>
2021-07-20 21:57:07 -0400

    Added drop down menu for miles radius and radio buttons

--- app/src/main/java/com/example/restaurantpicker/LocationActivity.java
+++ app/src/main/java/com/example/restaurantpicker/LocationActivity.java
@@ -3,12 +3,20 @@ package com.example.restaurantpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LocationActivity extends AppCompatActivity {
+    private Spinner spinnerMilesRadius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        spinnerMilesRadius = findViewById(R.id.spinnerMilesRadius);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.milesRadius, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMilesRadius.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/restaurantpicker/LoginActivity.java
+++ app/src/main/java/com/example/restaurantpicker/LoginActivity.java
@@ -62,7 +62,8 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            Intent intent = new Intent(LoginActivity.this, LocationActivity.class);
                             startActivity(intent);
                         } else {
                             Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
