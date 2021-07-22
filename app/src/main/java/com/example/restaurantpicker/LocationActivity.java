9ea56947801157478c2e0cc839e0d4847ba21bd3
buc0025 <stanley.yang001@gmail.com>
2021-07-22 18:16:20 -0400

    Clicking location button checks valid zip code and goes to cuisine

--- app/src/main/java/com/example/restaurantpicker/LocationActivity.java
+++ app/src/main/java/com/example/restaurantpicker/LocationActivity.java
@@ -1,15 +1,19 @@
 package com.example.restaurantpicker;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LocationActivity extends AppCompatActivity {
     private Spinner spinnerMilesRadius;
     private Button btnLocation;
+    private EditText edtZipCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,34 @@ public class LocationActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMilesRadius.setAdapter(adapter);
 
+        btnLocation = findViewById(R.id.btnLocation);
+        edtZipCode = findViewById(R.id.edtZipCode);
 
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String zipCode = edtZipCode.getText().toString();
+                if (zipCode.length() != 5) {
+                    zipCodeDialog();
+                } else {
+                    Intent intent = new Intent(LocationActivity.this, CuisineActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    private void zipCodeDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("Zip code is invalid.")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .create().show();
     }
 
     @Override
