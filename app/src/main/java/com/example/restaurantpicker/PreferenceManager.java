2133b67814125d08705403103e2f59122ed18a68
buc0025 <stanley.yang001@gmail.com>
2021-08-23 22:10:39 -0400

    Shared pref for saving restaurants no longer saves list

--- app/src/main/java/com/example/restaurantpicker/PreferenceManager.java
+++ app/src/main/java/com/example/restaurantpicker/PreferenceManager.java
@@ -10,10 +10,14 @@ import java.util.ArrayList;
 public class PreferenceManager {
 
     final private static String USER_PREFS = "USER_PREFS";
+    final private static String USER_RESTAURANTS = "USER_RESTAURANTS";
+
     private SharedPreferences sharedPreferences;
+    private SharedPreferences restaurantPreferences;
 
     public PreferenceManager(Context context) {
         sharedPreferences = context.getSharedPreferences(USER_PREFS, Context.MODE_PRIVATE);
+        restaurantPreferences = context.getSharedPreferences(USER_RESTAURANTS, Context.MODE_PRIVATE);
     }
 
     public void savePrefs(UserPreferences userPreferences, String uId) {
@@ -42,18 +46,26 @@ public class PreferenceManager {
         editor.apply();
     }
 
-    //Need to add restaurants to UserRestaurants before using this method
-    public ArrayList<Restaurant> getRestaurant(String uId) {
-        ArrayList<Restaurant> restaurants = new ArrayList<>();
+    // Shared preference will have one restaurant as value and its name as the key
+    public void saveRestaurant(String name, Restaurant restaurant) {
+        SharedPreferences.Editor editor = restaurantPreferences.edit();
         Gson gson = new Gson();
-        String json = sharedPreferences.getString(uId, null);
-        Restaurant obj = gson.fromJson(json, Restaurant.class);
-
-        for (Restaurant restaurant : obj)
-            //test push
-
-
+        String json = gson.toJson(restaurant);
+        editor.putString(name, json);
+        editor.apply();
     }
+
+    //Need to add restaurants to UserRestaurants before using this method
+//    public ArrayList<Restaurant> getRestaurant(String uId) {
+//        ArrayList<Restaurant> restaurants = new ArrayList<>();
+//        Gson gson = new Gson();
+//        String json = sharedPreferences.getString(uId, null);
+//        Restaurant obj = gson.fromJson(json, Restaurant.class);
+//
+//        for (Restaurant restaurant : obj)
+//
+//
+//    }
 
     public void removeRestaurants(String uId) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
